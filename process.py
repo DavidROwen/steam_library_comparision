e88673e829d857b14b78f878c986cb4032cfc167
David Owen <david.owen.dev@gmail.com>
2019-08-29 16:31:02 -0600

    Naming pass for clairity

--- process.py
+++ process.py
@@ -7,32 +7,35 @@ import os
 def parse():
     path = 'data/'
     folder = os.fsencode(path)
-    rows = []
+    users_games = []
 
+    # Go through each html file and strip the titles
     for file in os.listdir(folder):
         filename = os.fsdecode(file)
         if filename.endswith( ('.html') ):
             with open(path + filename) as fp:
                 soup = BeautifulSoup(fp, features='lxml')
             your_rows = soup.find_all(class_='gameListRowItemName')
-            titles = []
-            for row in your_rows:
-                titles.append(row.text)
-            rows.append(titles)
+            game_titles = []
+            for game in your_rows:
+                game_titles.append(game.text)
+            users_games.append(game_titles)
     
-    intersect = rows[0]
-    for title_list in rows:
+    # Merge the lists by intersection
+    intersect = users_games[0]
+    for title_list in users_games:
         intersect = intersection(intersect, title_list)
 
+    # Save the file
     with open('results.html', 'w') as f:
         for item in intersect:
             f.write(str("%s\n" % item))
     
 def intersection(list1, list2):
     temp_list = []
-    for item1 in list1:
-        if item1 in list2:
-            temp_list.append(item1)
+    for item in list1:
+        if item in list2:
+            temp_list.append(item)
     return temp_list
 
 parse()
